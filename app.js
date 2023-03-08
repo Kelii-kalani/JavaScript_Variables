f9d0baf20af9749466cf506450711530e5961322
Michael <keliikalani87@gmail.com>
2023-03-08 15:41:11 -0800

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Michael";
+let lastName = "Hartman";
+var age = 35;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+Console.Log(`${language} was created in ${createdYear}.`);
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+Console.Log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+Console.Log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`);
